34da362980590ed2ad5449ddf43d13c5eeae24e7
standartStan <stas.zoria@gmail.com>
2016-03-29 14:44:17 +0300

    fixed

--- src/test/java/com/main/BuggySuiteTest.java
+++ src/test/java/com/main/BuggySuiteTest.java
@@ -6,9 +6,7 @@ import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-/**
- * Created by stan on 29.03.16.
- */
+
 @RunWith(Categories.class)
 @Suite.SuiteClasses(TodosOperationsAtAllFilterTest.class)
 @Categories.IncludeCategory(Buggy.class)
--- src/test/java/com/main/features/BaseTest.java
+++ src/test/java/com/main/features/BaseTest.java
@@ -2,9 +2,7 @@ package com.main.features;
 
 import com.codeborne.selenide.Configuration;
 
-/**
- * Created by stan on 29.03.16.
- */
+
 public class BaseTest {
     {
         Configuration.browser = System.getProperty("driver.browser");
